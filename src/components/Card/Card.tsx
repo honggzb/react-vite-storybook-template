6c0919c68deb63a920ed221b89ce36d6aa20292a
Hong(Joanna) Qian <honggzb@users.noreply.github.com>
2024-06-26 16:12:31 -0400

    Create Card.tsx

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { options } from './constants'
+import './Card1.css'
+import ClassNames from 'ClassNames'
+
+const Card = ({
+  children,
+  color = 'primary',
+  size = 'sm',
+  isClickable,
+  isDragable,
+}) => {
+  return (
+    <div className={ ClassNames('card', {
+        [`color-${color}`]: color,
+        [`size-${size}`]: size,
+        'is-clickable': isClickable,
+        'is-dragable': isDragable,
+    })}>{ children }</div>
+  )
+}
+
+Card.propTypes = {
+  Children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(options.colors),
+  size: PropTypes.oneOf(options.sizes),
+  isClickable: PropTypes.bool,
+  isDragable: PropTypes.bool,
+}
+
+export default Card
